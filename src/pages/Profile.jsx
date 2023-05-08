b02162b6f0c1a110d1dcdb54500ea068016a3579
anass ebn erradiy <anass.ebn@gmail.com>
2023-05-08 18:57:33 +0100

    last commit anass

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,7 +6,8 @@ const Profile = ()=>{
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [image, setImage] = useState('');
+    const [uploadedImage, setUploadedImage] = useState('');
+    const [image,setImage] = useState() ;
   
     const handleSubmit = async (event) => {
       event.preventDefault();
@@ -15,57 +16,61 @@ const Profile = ()=>{
         dataRequest.append("name",name) ;
         dataRequest.append("email",email) ;
         dataRequest.append("password",password) ;
-        dataRequest.append("image",image) ;
+        dataRequest.append("image",uploadedImage) ;
+        // console.log(uploadedImage)
         const token = JSON.parse(localStorage.getItem("userInfo"))?.token;
         const {data} = await request.post('/updateProfile',dataRequest ,{headers :{'Authorization' : `Bearer ${token}`}});
-        console.log(data)
+        console.log(data.image_url) ;
+        localStorage.setItem('profile_image',data.image_url) ;
       } catch (error) {
-        console.log(error.message)
+        console.log(error.response.data.error)
       }
-      // logic to update user profile
     }
   
     const handleImageChange = (event) => {
+      setUploadedImage(event.target.files[0])
       const imageFile = event.target.files[0];
-      const reader = new FileReader();
-      reader.readAsDataURL(imageFile);
-      reader.onload = () => {
-        setImage(reader.result);
+      if(imageFile){
+        const reader = new FileReader();
+        reader.readAsDataURL(imageFile);
+        reader.onload = () => {
+          setImage(reader.result);
+        }
       }
     }
     return (
         <Container fluid className="my-5">
-        <Row className="justify-content-center">
-          <Col md={4} className="text-center">
-            <div className="mb-4">
-              <img src={image || "profile-image.jpg"} height="140px" alt="Profile Image" className="rounded-circle shadow w-75 mb-3" />
-              <Form.Group controlId="formBasicImage">
-                <Form.Control type="file" onChange={handleImageChange} className="form-control-file text-center" />
-              </Form.Group>
-            </div>
-          </Col>
-          <Col md={6}>
-            <h3 className="mb-4">Profile Information</h3>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group controlId="formBasicName">
-                <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter your name" value={name} onChange={(event) => setName(event.target.value)} />
-              </Form.Group>
-  
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" value={email} onChange={(event) => setEmail(event.target.value)} />
-              </Form.Group>
-  
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)} />
-              </Form.Group>
-  
-              <Button variant="primary" type="submit">Update Profile</Button>
-            </Form>
-          </Col>
-        </Row>
+            <Form onSubmit={handleSubmit} encType="multipart/form-data">
+                <Row className="justify-content-center">
+                <Col md={4} className="text-center">
+                    <div className="mb-4">
+                    <img src={image || "profile-image.jpg"} height="140px" alt="Profile Image" className="rounded shadow w-75 mb-3" />
+                    <Form.Group controlId="formBasicImage">
+                        <Form.Control type="file" name='image' onChange={handleImageChange} className="form-control-file text-center" />
+                    </Form.Group>
+                    </div>
+                </Col>
+                <Col md={6}>
+                        <h3 className="mb-4">Profile Information</h3>
+                    <Form.Group controlId="formBasicName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" name="name" placeholder="Enter your name" value={name} onChange={(event) => setName(event.target.value)} />
+                    </Form.Group>
+        
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" name="email" placeholder="Enter email" value={email} onChange={(event) => setEmail(event.target.value)} />
+                    </Form.Group>
+        
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" name="password" required placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)} />
+                    </Form.Group>
+        
+                    <Button variant="primary" type="submit">Update Profile</Button>
+                </Col>
+            </Row>
+        </Form>
       </Container>
     );
   }
